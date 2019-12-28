61324a1b1131b6a3f094169249fb424c4f895600
Domingo Javier Martinez Torrivilla <Domingo_Martinez@Dell.com>
2019-12-28 16:41:04 -0600

    Adding configuration

--- ConfiguringApps/Controllers/HomeController.cs
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using ConfiguringApps.Infrastructure;
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
+        private UptimeService uptime;
 
-        public ViewResult Index() => View(new Dictionary<string, string> {
-            ["Message"] = "This is the Index action"});
+        public HomeController(UptimeService uptime)
+        {
+            this.uptime = uptime;
+        }
+
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Index action",
+            ["Uptime"] = $"{uptime.Uptime}ms"
+        });
 
     }
 }
--- ConfiguringApps/Program.cs
+++ ConfiguringApps/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Reflection;
 
 namespace ConfiguringApps
 {
@@ -19,6 +14,9 @@ namespace ConfiguringApps
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>();
     }
 }
