3af5d2ac16d8b127355354c6a8291dc11d9c9f4a
Domingo Javier Martinez Torrivilla <Domingo_Martinez@Dell.com>
2019-12-28 15:02:45 -0600

    Add project files.

---
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index() => View(new Dictionary<string, string> {
+            ["Message"] = "This is the Index action"});
+
+    }
+}
